a5e811748d381069a7244f41db9949a29f60fa12
pgarcia97 <pgarcia@smith.edu>
2018-11-26 21:54:09 -0500

    - changed email and password to be placeholder text, also set-up the database and have successful relationship between Xcode code (inside LoginView...swift)

--- FiveCollegeCS/passwordViewController.swift
+++ FiveCollegeCS/passwordViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import SQLite3
 
 class passwordViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+   
         // Do any additional setup after loading the view.
     }
 
