52875d3944793ac1a6a62af6238010020e2b0ffe
pgarcia97 <pgarcia@smith.edu>
2018-11-27 23:21:24 -0500

    filter unpicked is now populated with school names

--- FiveCollegeCS/FilterViewController.swift
+++ FiveCollegeCS/FilterViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class FilterViewController: UIViewController {
+class FilterViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource  {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var colleges: UIPickerView!
+    
+    private let schoolComponent = 0
+    
+    private let schoolTypes = [
+        "UMass Amherst", "Amherst College", "Smith College", "Mt. Holyoke College",
+        "Hampshire College"]
+ 
+    // MARK:-
+    // MARK: Picker Data Source Methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return schoolTypes.count
     }
+    
+    // MARK:-
+    // MARK: Picker Delegate Methods
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component:
+        Int) -> String? {
+            return schoolTypes[row]
+    }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
     
 
     /*
