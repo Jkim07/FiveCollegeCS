0abf5a8b52eaf59ea4368ff070abc3b4e7b306f5
pgarcia97 <pgarcia@smith.edu>
2018-12-04 22:53:34 -0500

    updated database to contain first name and last name to user information.

--- FiveCollegeCS/SignUpViewController.swift
+++ FiveCollegeCS/SignUpViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-    var email:String = ""
+    //var email:String = ""
     @IBOutlet weak var emailInput: UITextField!
     @IBOutlet weak var passwordInput: UITextField!
+    
+    @IBOutlet weak var firstNameInput: UITextField!
+    @IBOutlet weak var lastNameInput: UITextField!
+    
     @IBAction func doneButton(_ sender: Any) {
         
         let inputEmail=emailInput.text
         let inputPassword=passwordInput.text
+        let inputFirstName=firstNameInput.text
+        let inputLastName=lastNameInput.text
         
-        let insertStatementString = "INSERT INTO users (username, password) VALUES (?, ?);"
+        let insertStatementString = "INSERT INTO users (username, password, first_name, last_name) VALUES (?, ?, ?, ?);"
         
         func insert() {
             var insertStatement: OpaquePointer? = nil
@@ -26,9 +32,14 @@ class SignUpViewController: UIViewController {
             if sqlite3_prepare_v2(db, insertStatementString, -1, &insertStatement, nil) == SQLITE_OK {
                 let user: NSString? = inputEmail as! NSString
                 let pass: NSString? = inputPassword as! NSString
+                let first: NSString? = inputFirstName as! NSString
+                let last: NSString? = inputLastName as! NSString
+                
                 // 3
                 sqlite3_bind_text(insertStatement, 1, user?.utf8String, -1, nil)
                 sqlite3_bind_text(insertStatement, 2, pass?.utf8String, -1, nil)
+                sqlite3_bind_text(insertStatement, 3, first?.utf8String, -1, nil)
+                sqlite3_bind_text(insertStatement, 4, last?.utf8String, -1, nil)
                 
                 // 4
                 if sqlite3_step(insertStatement) == SQLITE_DONE {
