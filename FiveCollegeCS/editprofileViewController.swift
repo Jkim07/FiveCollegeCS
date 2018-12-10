952cddffad5a0fa43078ef684d2dd73bb71ee76d
Ju Young Kim <juyoungkim08@gmail.com>
2018-12-10 07:12:18 -0500

    user profile image view accepts images from photo library

--- FiveCollegeCS/editprofileViewController.swift
+++ FiveCollegeCS/editprofileViewController.swift
@@ -1,31 +1,26 @@
 //
-//  ProfileViewController.swift
+//  editprofileViewController.swift
 //  FiveCollegeCS
 //
-//  Created by Julie Kim  on 10/28/18.
+//  Created by Julie Kim  on 12/10/18.
 //  Copyright © 2018 mikeythebadass. All rights reserved.
 //
 
 import UIKit
 
-class ProfileViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate{
-
+class editprofileViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
     @IBOutlet weak var profile_img: UIImageView!
-
     
-    
-    
-    @IBAction func editPhotoButtonPressed(_ sender: Any) {
+    @IBAction func importPhoto(_ sender: Any) {
         let image = UIImagePickerController()
         image.delegate = self
         image.sourceType = UIImagePickerControllerSourceType.photoLibrary
         image.allowsEditing = false
         self.present(image,animated:true)
         {
-         // after it is complete
-         // insert this new image into this user's account database
+            // after it is complete
+            // insert this new image into this user's account database
         }
-        
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]){
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
@@ -35,14 +30,11 @@ class ProfileViewController: UIViewController,UINavigationControllerDelegate,UII
         }
         self.dismiss(animated:true, completion:nil)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //if user's profile image is null
         profile_img.image = #imageLiteral(resourceName: "clear-robot")
-        //otherwise load the profile image
-        //userimage =
-        //profile_img.image = userimage
-        
+
         // Do any additional setup after loading the view.
     }
 
